a72c918afd9a576f22ff02121a8112185b52b13b
Rafael De La Cruz <rafael_delacruz1992@yahoo.com>
2020-04-09 15:04:08 +0400

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,6 +19,7 @@
       backDelay: 500,
     });
   }
+  
 
   // Smooth scroll for the navigation menu and links with .scrollto classes
   $(document).on("click", ".nav-menu a, .scrollto", function (e) {
